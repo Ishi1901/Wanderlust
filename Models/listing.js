9c516a08e1f141784ec4b469aa3b0a53c2e50ded
Ishi1901 <ishitamore1905@gmail.com>
2026-08-20 15:38:35 +0530

    listing model added

---
+++ Models/listing.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required : true
+    },
+    description: String,
+    // image: {
+    //     type:String,
+    //     default: "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    //     set: (v) => v ===""? "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" :v
+    // },
+    image: {
+    filename: String,
+    url: String,
+  },
+    price:Number,
+    location:String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
