bfa0cbd4dcbf070cd3ce4816e89446a6c9e6905d
Ishi1901 <ishitamore1905@gmail.com>
2026-08-20 15:39:09 +0530

    data added

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../Models/listing');
+
+
+URL_MONGO = "mongodb://127.0.0.1:27017/Wanderlust";
+
+
+main().then(()=>{
+    console.log("Database connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+
+async function main() {
+  await mongoose.connect(URL_MONGO);
+};
+
+const initDB = async () =>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialised");
+
+};
+
+initDB();
